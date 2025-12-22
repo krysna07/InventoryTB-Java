800fc9b1837ec247e6eb56c95270a40719eca991
Dika Pradnyanta <64448579+dikapradnyanta@users.noreply.github.com>
2025-12-23 04:57:08 +0800

    Perbaikan bug database dengan inisialisasi konfigurasi default

--- src/InventoryTB/InventoryTB.java
+++ src/InventoryTB/InventoryTB.java
@@ -17,8 +17,12 @@ public class InventoryTB {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        KoneksiDB.getConnection();
+        KoneksiDB.getConnection(); // Setup database
+        
+        // Langsung tampilkan login
+        java.awt.EventQueue.invokeLater(() -> {
+            new FormLogin().setVisible(true);
+        });
         
     }
     
